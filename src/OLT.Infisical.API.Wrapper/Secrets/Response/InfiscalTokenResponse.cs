e910bc6984aa78ccc0273fabb056af878618af49
Chris Straw <chris.straw@contractors.courts.in.gov>
2025-10-02 13:57:30 -0400

    Minor Cleanup

--- src/OLT.Infisical.API.Wrapper/Secrets/Response/InfiscalTokenResponse.cs
+++ src/OLT.Infisical.API.Wrapper/Secrets/Response/InfiscalTokenResponse.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System.Text.Json;
-
-namespace OLT.Infisical.API.Wrapper.Secrets.Response;
+﻿namespace OLT.Infisical.API.Wrapper.Secrets.Response;
 
 public sealed class InfiscalTokenResponse
 {
@@ -18,27 +15,17 @@ public sealed class InfiscalTokenResponse
         }
     }
 
-    public DateTime GetExpiryTimestamp()
+    public DateTimeOffset GetExpiryTimestamp()
     {
         try
         {
-            if (string.IsNullOrWhiteSpace(AccessToken))
-                return DateTime.MinValue;
-            if (!AccessToken.Contains("."))
-                return DateTime.MinValue;
-
-            string[] parts = AccessToken.Split('.');
-            InfisicalJwtToken? payload = JsonSerializer.Deserialize<InfisicalJwtToken>(Base64UrlEncoder.Decode(parts[1]));
-
-            if (payload == null) return DateTime.MinValue;
-
-            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(payload.Exp);
-            return dateTimeOffset.LocalDateTime;
+            return DateTimeOffset.FromUnixTimeSeconds(ExpiresIn).AddSeconds(-30);
         }
-        catch (Exception)
-        {
-            return DateTime.MinValue;
+        catch 
+        { 
+            return DateTimeOffset.Now.AddMinutes(-10);
         }
+      
     }
 
 }
